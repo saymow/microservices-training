d8f0daee5c26328e602f5c6019292bc6c4c27695
Gustavo <gustavo_alves2010@yahoo.com.br>
2022-08-30 11:23:56 -0300

    feat: posts service

---
+++ posts/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+const posts = {};
+
+const randomId = () => Math.random().toString("16");
+
+app.use(express.json());
+
+app.get("/posts", (req, res) => {
+  return res.send(posts);
+});
+
+app.post("/posts", (req, res) => {
+  const { title } = req.body;
+  const id = randomId();
+
+  posts[id] = { id, title };
+
+  return res.status(201).send(posts);
+});
+
+app.listen(4000, () => console.log("Listening on port 4000."));
