d0eb8c86acd35a2dd7974e1a7d5aadd01f4cd2ad
Gustavo <gustavo_alves2010@yahoo.com.br>
2022-08-30 11:24:08 -0300

    feat: comments service

---
+++ comments/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+const commentsByPostId = {};
+
+const randomId = () => Math.random().toString("16");
+
+app.use(express.json());
+
+app.get("/posts/:id/comments", (req, res) => {
+  return res.send(commentsByPostId[req.params.id] ?? []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const { content } = req.body;
+  const postId = req.params.id;
+  const id = randomId();
+
+  if (!(postId in commentsByPostId)) {
+    commentsByPostId[postId] = [];
+  }
+
+  commentsByPostId[postId].push({ id, content });
+
+  return res.status(201).send(commentsByPostId[postId]);
+});
+
+app.listen(4001, () => console.log("Listening on port 4001."));
