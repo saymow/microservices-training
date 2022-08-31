6c2a83bdf1511cf9d2bcb5d034561aad4954b1f7
Gustavo <gustavo_alves2010@yahoo.com.br>
2022-08-31 10:24:28 -0300

    feat: async communication between services

---
+++ query/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const posts = {};
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/posts", async (req, res) => {
+  return res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, payload } = req.body;
+
+  if (type === "CREATED_POST") {
+    const { id, title } = payload;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CREATED_COMMENT") {
+    const { postId, comment } = payload;
+
+    posts[postId].comments.push(comment);
+  }
+
+  console.log(posts);
+
+  return res.status(201).send(posts);
+});
+
+app.listen(4002, () => console.log("Listening on port 4002."));
