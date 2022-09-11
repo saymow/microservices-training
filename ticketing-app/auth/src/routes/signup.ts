8163c2d16dbfe0146f73f6700b7ddf779a5c667a
Gustavo <gustavo_alves2010@yahoo.com.br>
2022-09-11 20:25:33 -0300

    feat: auth add validation

--- ticketing-app/auth/src/routes/signup.ts
+++ ticketing-app/auth/src/routes/signup.ts
@@ -1,9 +1,28 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = Router();
 
-router.post("/signup", (req, res) => {
-  res.send("to-do");
-});
+router.post(
+  "/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters "),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array())
+    }
+
+    const { email, password } = req.body;
+
+    res.send("to-dodd");
+  }
+);
 
 export { router as signUpRouter };
