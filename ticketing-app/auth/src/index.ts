15d9777df0084319ac50f17e7dfaabbebc899940
Gustavo <gustavo_alves2010@yahoo.com.br>
2022-09-11 12:01:46 -0300

    feat: add k8s ingress-service setup

--- ticketing-app/auth/src/index.ts
+++ ticketing-app/auth/src/index.ts
@@ -1,8 +1,11 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hi there!");
+});
 
-app.listen(3000, () => console.log('Listening on port 3000.'))
+app.listen(3000, () => console.log("Listening on port 3000."));
