279921456f39f1927dc6647002bbf5ca525ea86f
Gustavo <gustavo_alves2010@yahoo.com.br>
2022-09-11 20:43:27 -0300

    feat: auth add error handler middleware

--- ticketing-app/auth/src/index.ts
+++ ticketing-app/auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { errorHandler } from "./middlewares/error-handler";
 import * as routes from "./routes";
 
 const app = express();
@@ -8,5 +9,7 @@ app.use(express.json());
 Object.values(routes).forEach((route) => {
   app.use("/api/users", route);
 });
+
+app.use(errorHandler);
 
 app.listen(3000, () => console.log("Listening on port 3000."));
