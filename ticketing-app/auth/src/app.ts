bcc8e64f03f5bf7241880c27c26f6df6677853b4
Gustavo <gustavo_alves2010@yahoo.com.br>
2022-09-19 11:12:05 -0300

    refactor: auth split app and initialization logic

---
+++ ticketing-app/auth/src/app.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import "express-async-errors";
+import mongoose from "mongoose";
+import cookieSession from "cookie-session";
+import * as routes from "./routes";
+import { errorHandler } from "./middlewares/error-handler";
+import { RouteNotFoundError } from "./errors";
+import { ensureEnvVariables } from "./env";
+
+const app = express();
+
+ensureEnvVariables();
+app.set("trust proxy", true);
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+Object.values(routes).forEach((route) => {
+  app.use("/api/users", route);
+});
+
+app.all("*", (req) => {
+  throw new RouteNotFoundError(req.originalUrl);
+});
+app.use(errorHandler);
+
+export { app };
--- ticketing-app/auth/src/index.ts
+++ ticketing-app/auth/src/index.ts
@@ -1,35 +1,8 @@
-import express from "express";
-import "express-async-errors";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-import * as routes from "./routes";
-import { errorHandler } from "./middlewares/error-handler";
-import { RouteNotFoundError } from "./errors";
-import { ensureEnvVariables } from "./env";
-
-const app = express();
-
-app.set("trust proxy", true);
-app.use(express.json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-Object.values(routes).forEach((route) => {
-  app.use("/api/users", route);
-});
-
-app.all("*", (req) => {
-  throw new RouteNotFoundError(req.originalUrl);
-});
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
   try {
-    ensureEnvVariables();
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
     console.log("Connected to mongodb");
     app.listen(3000, () => console.log("Listening on port 3000."));
