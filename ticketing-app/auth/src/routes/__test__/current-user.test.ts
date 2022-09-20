d2958fa848ed11bb01c8f48237c3b07333af6d7b
Gustavo <gustavo_alves2010@yahoo.com.br>
2022-09-20 11:10:21 -0300

    refactor: add auth global helper on test environment

--- ticketing-app/auth/src/routes/__test__/current-user.test.ts
+++ ticketing-app/auth/src/routes/__test__/current-user.test.ts
@@ -3,17 +3,11 @@ import { app } from "../../app";
 
 describe("CurrentUser Route", () => {
   it("Should return current user details on success", async () => {
-    const authResponse = await request(app)
-      .post("/api/users/signup")
-      .send({
-        email: "valid-email@mail.com",
-        password: "valid-password",
-      })
-      .expect(201);
+    const cookie = await global.auth();
 
     const response = await request(app)
       .get("/api/users/currentuser")
-      .set("Cookie", authResponse.get("Set-Cookie"))
+      .set("Cookie", cookie)
       .expect(200);
 
     expect(response.body.currentUser.email).toEqual("valid-email@mail.com");
--- ticketing-app/auth/src/test/setup.ts
+++ ticketing-app/auth/src/test/setup.ts
@@ -1,9 +1,14 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
 
 process.env.JWT_KEY = "jwt-key";
 
 import { app } from "../app";
+
+declare global {
+  var auth: () => Promise<string[]>;
+}
 
 let mongo: any;
 beforeAll(async () => {
@@ -27,3 +32,15 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 });
+
+global.auth = async () => {
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "valid-email@mail.com",
+      password: "valid-password",
+    })
+    .expect(201);
+
+  return response.get("Set-Cookie");
+};
