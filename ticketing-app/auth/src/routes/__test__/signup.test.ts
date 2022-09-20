664671eae9eb58fd2f9d903141071d668950f3c1
Gustavo <gustavo_alves2010@yahoo.com.br>
2022-09-20 10:19:10 -0300

    test: ensure signup returns 400 if email and password are not provided

--- ticketing-app/auth/src/routes/__test__/signup.test.ts
+++ ticketing-app/auth/src/routes/__test__/signup.test.ts
@@ -12,7 +12,7 @@ describe("SignUp Route", () => {
       .expect(201);
   });
 
-  it("Should returns 400 if email is invalid", async () => {
+  it("Should return 400 if email is invalid", async () => {
     return request(app)
       .post("/api/users/signup")
       .send({
@@ -21,14 +21,26 @@ describe("SignUp Route", () => {
       })
       .expect(400);
   });
-  
-  it("Should returns 400 if password is invalid", async () => {
+
+  it("Should return 400 if password is invalid", async () => {
     return request(app)
       .post("/api/users/signup")
       .send({
         email: "valid-email@mail.com",
         password: "x",
       })
+      .expect(400);
+  });
+
+  it("Should return 400 if email and password are not provided", async () => {
+    await request(app)
+      .post("/api/users/signup")
+      .send({ email: "valid-email@mail.com" })
+      .expect(400);
+    
+      await request(app)
+      .post("/api/users/signup")
+      .send({ password: "valid-password" })
       .expect(400);
   });
 });
