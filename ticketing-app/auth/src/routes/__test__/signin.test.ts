6de131abb4eef2671277e217b4211f86a6b6fecb
Gustavo <gustavo_alves2010@yahoo.com.br>
2022-09-20 10:37:18 -0300

    test: ensure signin returns 400 if incorrect password is provided

--- ticketing-app/auth/src/routes/__test__/signin.test.ts
+++ ticketing-app/auth/src/routes/__test__/signin.test.ts
@@ -11,4 +11,22 @@ describe("SignIn Route", () => {
       })
       .expect(400);
   });
+
+  it("Should return 400 if incorrect password is provided", async () => {
+    await request(app)
+      .post("/api/users/signup")
+      .send({
+        email: "valid-email@mail.com",
+        password: "valid-password",
+      })
+      .expect(201);
+
+    return request(app)
+      .post("/api/users/signin")
+      .send({
+        email: "valid-email@mail.com",
+        password: "invalid-password",
+      })
+      .expect(400);
+  });
 });
