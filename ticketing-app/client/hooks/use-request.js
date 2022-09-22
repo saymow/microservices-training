011df01e9482ad4bb63ab45b700d65df94a83ea3
Gustavo <gustavo_alves2010@yahoo.com.br>
2022-09-22 12:04:40 -0300

    feat: client add useRequest hook

---
+++ ticketing-app/client/hooks/use-request.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import axios from "axios";
+
+const useRequest = ({ url, method, body }) => {
+  const [errors, setErrors] = useState(null);
+
+  async function execute() {
+    try {
+      setErrors(null);
+      const response = await axios[method](url, body);
+
+      return response.data;
+    } catch (err) {
+      console.error(err);
+      setErrors(
+        <div className="alert alert-danger">
+          <h4>Oooops...</h4>
+          <ul className="my-0">
+            {err.response.data.errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  }
+
+  return { execute, errors };
+};
+
+export default useRequest;
--- ticketing-app/client/pages/auth/signup.js
+++ ticketing-app/client/pages/auth/signup.js
@@ -1,39 +1,18 @@
 import { useState } from "react";
-import axios from "axios";
-
-function FieldErrors(props) {
-  const { errors, field } = props;
-  const fieldErrors = errors.filter((err) => err.field === field);
-
-  if (!fieldErrors.length) return null;
-
-  return (
-    <ul class="alert alert-danger my-1">
-      {fieldErrors.map((fieldError) => (
-        <li key={fieldError.message}>{fieldError.message}</li>
-      ))}
-    </ul>
-  );
-}
+import useRequest from "../../hooks/use-request";
 
 function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
+  const { execute, errors } = useRequest({
+    url: "/api/users/signup",
+    method: "post",
+    body: { email, password },
+  });
 
   async function handleSubmit(e) {
     e.preventDefault();
-    try {
-      const response = await axios.post("/api/users/signup", {
-        email,
-        password,
-      });
-
-      console.log(response.data);
-    } catch (err) {
-      console.log(err);
-      setErrors(err.response.data.errors);
-    }
+    await execute();
   }
 
   return (
@@ -47,7 +26,6 @@ function Signup() {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-        <FieldErrors errors={errors} field={"email"} />
       </div>
       <div className="form-group">
         <label>Password</label>
@@ -57,8 +35,9 @@ function Signup() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <FieldErrors errors={errors} field={"password"} />
       </div>
+
+      {errors && <div className="m-2">{errors}</div>}
       <button className="btn btn-primary">Sign up</button>
     </form>
   );
