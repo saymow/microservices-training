1fbd8ead7160f5107e5fd8897ef3027178c82743
Gustavo <gustavo_alves2010@yahoo.com.br>
2022-09-23 12:06:42 -0300

    feat: client fetching data and forwarding cookies using SSR

--- ticketing-app/client/pages/index.js
+++ ticketing-app/client/pages/index.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import axios from "axios";
+import React from "react";
 
-function LandingPage() {
-    return <h1>Landing Page 5</h1>
+function LandingPage({ currentUser }) {
+  console.log(currentUser);
+
+  return <h1>Landing Page 5</h1>;
 }
 
-export default LandingPage
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === "undefined") {
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: req.headers,
+      }
+    );
+
+    return data;
+  } else {
+    const { data } = await axios.get("/api/users/currentuser");
+
+    return data;
+  }
+};
+
+export default LandingPage;
