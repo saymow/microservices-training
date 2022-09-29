8f398640378d5c05b61623b58f39b1d6b5f66ba0
Gustavo <gustavo_alves2010@yahoo.com.br>
2022-09-29 20:56:21 -0300

    test: ensure api/tickets is defined

---
+++ ticketing-app/tickets/src/routes/__tests__/new.test.ts
@@ -0,0 +1,18 @@
+import request from "supertest";
+import { app } from "../../app";
+
+describe("api/tickets Route", () => {
+  it("Should be defined", async () => {
+    const response = await request(app).post("/api/tickets").send({});
+
+    expect(response.statusCode).not.toBe(404);
+  });
+
+  it("Should should have authentication", async () => {});
+
+  it("Should returns an error on invalid input (title)", async () => {});
+
+  it("Should returns an error on invalid input (price)", async () => {});
+
+  it("Should should create a ticket on success", async () => {});
+});
