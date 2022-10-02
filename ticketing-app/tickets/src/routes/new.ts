b335192253c58049a17b909536a3e463436980ce
Gustavo <gustavo_alves2010@yahoo.com.br>
2022-10-01 21:00:48 -0300

    feat: ensure api/tickets have validation

--- ticketing-app/tickets/src/routes/new.ts
+++ ticketing-app/tickets/src/routes/new.ts
@@ -1,11 +1,20 @@
 import express, { Request, Response } from "express";
-import { currentUser, authentication } from "@saymowtickets/common";
+import { withValidation, authentication } from "@saymowtickets/common";
+import { body } from "express-validator";
 
 const router = express.Router();
 
 router.post(
   "/tickets",
   authentication,
+  ...withValidation(
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("price")
+      .isFloat({
+        gt: 0,
+      })
+      .withMessage("Price must be greater than 0")
+  ),
   (req: Request, res: Response) => {
     res.sendStatus(200);
   }
