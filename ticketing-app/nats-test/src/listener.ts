646f728bfbaf95fd8fd76b36583da744b0d64451
Gustavo <gustavo_alves2010@yahoo.com.br>
2022-10-09 10:48:56 -0300

    feat: add event redelivery/durable subscriptions

--- ticketing-app/nats-test/src/listener.ts
+++ ticketing-app/nats-test/src/listener.ts
@@ -3,17 +3,37 @@ import { randomBytes } from "crypto";
 
 console.clear();
 
-const client = nats.connect("ticketing", randomBytes(4).toString('hex'), {
+const client = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
 client.on("connect", () => {
   console.log("listener connected");
 
-  const subscription = client.subscribe("ticket:created", "listener-queue-group");
+  const option = client
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('accounting-service');
+  const subscription = client.subscribe(
+    "ticket:created",
+    "listener-queue-group",
+    option
+  );
+
+  subscription.on("close", () => {
+    console.log("NATS process terminated.");
+    process.exit();
+  });
 
   subscription.on("message", (msg) => {
     const [seq, data] = [msg.getSequence(), msg.getData()];
+
     console.log(`Received event ${seq}: ${data}`);
+
+    msg.ack();
   });
 });
+
+process.on("SIGINT", () => client.close());
+process.on("SIGTERM", () => client.close());
