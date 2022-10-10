50851ead5e262fb0877561a7587fc863ed7fb614
Gustavo <gustavo_alves2010@yahoo.com.br>
2022-10-10 11:56:49 -0300

    feat: NatsWrapper graceful shutdown

--- ticketing-app/tickets/src/index.ts
+++ ticketing-app/tickets/src/index.ts
@@ -6,6 +6,14 @@ import { natsWrapper } from "./nats-wrapper";
 const start = async () => {
   try {
     await natsWrapper.connect("ticketing", "adjasiop", "http://nats-srv:4222");
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
     await mongoose.connect(ENV.MONGO_URI);
     console.log("Connected to mongodb");
     app.listen(3000, () => console.log("Listening on port 3000."));
