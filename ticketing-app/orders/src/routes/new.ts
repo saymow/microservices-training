0aed68baae46f86b226501ec0b33757a95eae570
Gustavo <gustavo_alves2010@yahoo.com.br>
2022-10-12 12:04:26 -0300

    feat: ticket model

--- ticketing-app/orders/src/routes/new.ts
+++ ticketing-app/orders/src/routes/new.ts
@@ -1,9 +1,21 @@
+import { authentication, withValidation } from "@saymowtickets/common";
 import express, { Request, Response } from "express";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/orders",
+  authentication,
+  ...withValidation(
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .withMessage("TicketId must be provided")
+  ),
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as PostOrderRouter };
