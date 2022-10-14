769c41b91c492a32e75c319c691ddfad848ac24c
Gustavo <gustavo_alves2010@yahoo.com.br>
2022-10-14 12:00:35 -0300

    test: ensure order creation

---
+++ ticketing-app/orders/src/routes/__tests__/new.test.ts
@@ -0,0 +1,52 @@
+import request from "supertest";
+import mongoose from "mongoose";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+import { natsWrapper } from "../../nats-wrapper";
+import { Order, OrderStatus } from "../../models/order";
+
+describe("Post api/orders Route", () => {
+  it("Should return an error if ticket does not exists", async () => {
+    const ticketId = new mongoose.Types.ObjectId();
+    await request(app)
+      .post("/api/orders")
+      .set("Cookie", global.auth())
+      .send({ ticketId })
+      .expect(404);
+  });
+
+  it("Should return an error if ticket is already reserved", async () => {
+    const ticket = Ticket.build({
+      title: "title",
+      price: 20,
+    });
+    await ticket.save();
+    const order = Order.build({
+      ticket,
+      userId: "some-id",
+      status: OrderStatus.Created,
+      expiresAt: new Date(),
+    });
+    await order.save();
+
+    await request(app)
+      .post("/api/orders")
+      .set("Cookie", global.auth())
+      .send({ ticketId: ticket.id })
+      .expect(400);
+  });
+
+  it("Should create an order on success", async () => {
+    const ticket = Ticket.build({
+      title: "title",
+      price: 20,
+    });
+    await ticket.save();
+
+    await request(app)
+      .post("/api/orders")
+      .set("Cookie", global.auth())
+      .send({ ticketId: ticket.id })
+      .expect(201);
+  });
+});
