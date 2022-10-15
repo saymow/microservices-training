7c246906490ac215e588e3d2925ca1ac33b85c9a
Gustavo <gustavo_alves2010@yahoo.com.br>
2022-10-15 11:06:34 -0300

    feat: ensure order fetching

--- ticketing-app/orders/src/routes/show.ts
+++ ticketing-app/orders/src/routes/show.ts
@@ -1,9 +1,20 @@
+import { RouteNotFoundError, UnauthorizedError } from "@saymowtickets/common";
 import express, { Request, Response } from "express";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
 router.get("/orders/:id", async (req: Request, res: Response) => {
-  res.send({});
+  const order = await Order.findById(req.params.id).populate("ticket");
+
+  if (!order) {
+    throw new RouteNotFoundError("");
+  }
+  if (order.userId !== req.currentUser!.id) {
+    throw new UnauthorizedError();
+  }
+
+  return res.send(order);
 });
 
 export { router as ShowOrderRouter };
