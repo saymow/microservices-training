31ed7fa2e02b1a15de33726d04695fed5937fd94
Gustavo <gustavo_alves2010@yahoo.com.br>
2022-10-15 11:24:27 -0300

    feat: ensure order cancelation

--- ticketing-app/orders/src/routes/delete.ts
+++ ticketing-app/orders/src/routes/delete.ts
@@ -1,9 +1,28 @@
+import {
+  OrderStatus,
+  RouteNotFoundError,
+  UnauthorizedError,
+} from "@saymowtickets/common";
 import express, { Request, Response } from "express";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
 router.delete("/orders/:id", async (req: Request, res: Response) => {
-  res.send({});
+  const { id } = req.params;
+  const order = await Order.findById(id);
+
+  if (!order) {
+    throw new RouteNotFoundError("");
+  }
+  if (order.userId !== req.currentUser!.id) {
+    throw new UnauthorizedError();
+  }
+
+  order.status = OrderStatus.Cancelled;
+  await order.save();
+
+  return res.status(204).send(order);
 });
 
 export { router as DeleteOrderRouter };
