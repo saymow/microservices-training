6735c9967c4e3476325f147215790d788a92c65d
Gustavo <gustavo_alves2010@yahoo.com.br>
2022-10-16 10:40:52 -0300

    feat: emit order deletion event

--- ticketing-app/orders/src/routes/delete.ts
+++ ticketing-app/orders/src/routes/delete.ts
@@ -4,13 +4,15 @@ import {
   UnauthorizedError,
 } from "@saymowtickets/common";
 import express, { Request, Response } from "express";
+import { OrderCancelledPublisher } from "../events/order-cancelled-publisher";
 import { Order } from "../models/order";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
 router.delete("/orders/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
-  const order = await Order.findById(id);
+  const order = await Order.findById(id).populate("ticket");
 
   if (!order) {
     throw new RouteNotFoundError("");
@@ -21,6 +23,13 @@ router.delete("/orders/:id", async (req: Request, res: Response) => {
 
   order.status = OrderStatus.Cancelled;
   await order.save();
+
+  await new OrderCancelledPublisher(natsWrapper.client).publish({
+    id,
+    ticket: {
+      id: order.ticket.id,
+    },
+  });
 
   return res.status(204).send(order);
 });
